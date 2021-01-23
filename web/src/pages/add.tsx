602918feee986ada22deff87c61e584ae665600b
msolek@ubuntu <maciejsolek@gmail.com>
2021-01-23 16:31:14 +0100

    x

--- web/src/pages/add.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-interface addProps {}
-const AddAnnouncement: React.FC<addProps> = ({}) => {
-  return <div> add page</div>;
-};
-
-export default AddAnnouncement;
--- web/src/pages/announcements.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-interface announcementsProps {}
-const Announcements: React.FC<announcementsProps> = ({}) => {
-  return <div> announcements page</div>;
-};
-
-export default Announcements;
